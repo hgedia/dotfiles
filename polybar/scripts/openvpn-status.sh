7dd37fa26c9e0c68c4433104ab99c02a8c90fb2c
hgedia <hgedia@hgedia.com>
2019-10-25 18:54:31 +0100

    Updating vpn script

--- polybar/scripts/openvpn-status.sh
+++ polybar/scripts/openvpn-status.sh
@@ -1,9 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 if pgrep -x "openvpn" > /dev/null
 then
     region=$(pgrep -a openvpn$ | head -n 1 | cut -d " " -f 4 | cut -d "." -f 1 | awk '{print toupper($0)}')
-    echo " VPN:UP:$region "
+    if [[ $region = *"PRITUNL"* ]]
+    then
+        echo " VPN:UP:PRITUNL "
+    else
+        echo " VPN:UP:$region "
+    fi
 else
     echo " VPN:DOWN "
 fi
